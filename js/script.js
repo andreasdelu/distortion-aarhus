b7e4b2d0f5dc1d81cf74e9dc034b5df823e14897
andreasdelu <Andreasdelu@gmail.com>
2022-05-12 12:48:09 +0200

    lol
    Co-authored-by: Runelc <Runelc@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ window.addEventListener("scroll", () => {
     outlines[1].style.top = (document.getElementById("billetter").offsetTop - scrollY+200)/34 + "%";
     outlines[2].style.top = (document.getElementById("kort").offsetTop - scrollY-200)/35 + "%";
 })
+
+const burger = document.getElementById('burger-container');
+const navmenu = document.getElementById('navmenu');
+
+burger.addEventListener("click", () => {
+    if (navmenu.style.display == "flex") {
+        burger.setAttribute("class", "after-menu");
+        document.body.style.overflowY = "auto"
+        navmenu.style.display = "none"
+        navmenu.style.right = "-100%"
+        document.getElementById("top-line").classList.remove("top-line")
+        document.getElementById("mid-line").classList.remove("mid-line")
+        document.getElementById("bot-line").classList.remove("bot-line")
+    }
+    else{
+        navmenu.style.display = "flex"
+        setTimeout(() => {
+            navmenu.style.right = "0"
+        }, 20);
+        document.body.style.overflowY = "hidden"
+        burger.setAttribute("class", "after-close");
+        document.getElementById("top-line").classList.add("top-line")
+        document.getElementById("mid-line").classList.add("mid-line")
+        document.getElementById("bot-line").classList.add("bot-line")
+    }
+})
