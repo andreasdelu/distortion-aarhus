8b3f5d6084fcede92aa6fac009c4fcfe315c9967
andreasdelu <Andreasdelu@gmail.com>
2022-05-16 14:07:22 +0200

    komprimerede billeder
    Co-authored-by: Runelc <Runelc@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -4,48 +4,54 @@ const karrusselBot = document.querySelector(".karrussel-bot")
 const ticketAnim = document.getElementById("ticketanim");
 const ctx = ticketAnim.getContext("2d");
 
+const program = document.getElementById('program');
+const billetter = document.getElementById('billetter');
+const kort = document.getElementById('kort');
+const merch = document.getElementById('merch');
+
 const scrollOffset = 400
 
 
 window.addEventListener("scroll", () => {
-    outlines[0].style.top = (document.getElementById("program").offsetTop - scrollY-200)/37 + "%";
-    outlines[1].style.top = (document.getElementById("billetter").offsetTop - scrollY+200)/34 + "%";
-    outlines[2].style.top = (document.getElementById("kort").offsetTop - scrollY-200)/35 + "%";
-    outlines[3].style.top = (document.getElementById("merch").offsetTop - scrollY+200)/34 + "%";
+    outlines[0].style.top = (program.offsetTop - scrollY-200)/37 + "%";
+    outlines[1].style.top = (billetter.offsetTop - scrollY+200)/34 + "%";
+    outlines[2].style.top = (kort.offsetTop - scrollY-200)/35 + "%";
+    outlines[3].style.top = (merch.offsetTop - scrollY+200)/34 + "%";
 
     karrusselTop.style.backgroundPositionX = 100 + scrollY/20 + "%"
     karrusselBot.style.backgroundPositionX = "-" + scrollY/20 + "%"
 
-    if (scrollY + scrollOffset >= document.getElementById("billetter").offsetTop) {
-        const diff = Math.floor((scrollY + scrollOffset - document.getElementById("billetter").offsetTop)/2.8)
+    if(scrollY + scrollOffset <= billetter.offsetTop){
+        requestAnimationFrame(updateCanvas(0))
+    }
+    if (scrollY + scrollOffset >= billetter.offsetTop) {
+        const diff = Math.floor((scrollY + scrollOffset - billetter.offsetTop)/2.8)
+        if(scrollY + scrollOffset > billetter.offsetTop + billetter.offsetHeight){
+            requestAnimationFrame(updateCanvas(141))
+            return;
+        }
         if (diff >= 0 && diff <= 142) {
             requestAnimationFrame(updateCanvas(diff))
         }
     }
-    if(scrollY + scrollOffset > document.getElementById("billetter").offsetTop + document.getElementById("billetter").offsetHeight){
-        requestAnimationFrame(updateCanvas(141))
-    }
-    if(scrollY + scrollOffset < document.getElementById("billetter").offsetTop){
-        requestAnimationFrame(updateCanvas(0))
-    }
 
 })
 
-let ticketUrl = "../images/ticketseq/ticketanim"
+let ticketUrl = "../images/ticketseq/webp/ticketanim"
 
 const frames = 142
 
 function preloadImages() {
     for (let i = 1; i < frames; i++) {
         const img = new Image()
-        img.src = ticketUrl + i.toString().padStart(3, "0") + ".jpg";
+        img.src = ticketUrl + i.toString().padStart(3, "0") + ".webp";
     }
 }
 
 preloadImages()
 
 const img = new Image()
-img.src = ticketUrl + "000.jpg"
+img.src = ticketUrl + "000.webp"
 ticketAnim.width = 500;
 ticketAnim.height = 500;
 img.onload = () => {
@@ -53,7 +59,7 @@ img.onload = () => {
 }
 
 function updateCanvas(i) {
-    img.src = ticketUrl + i.toString().padStart(3, "0") + ".jpg";
+    img.src = ticketUrl + i.toString().padStart(3, "0") + ".webp";
     img.onload = () => {
         ctx.drawImage(img, 0, 0)
     }
