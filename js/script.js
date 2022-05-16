d0de990ae9885c1727b55f4197481dbbc7990979
andreasdelu <Andreasdelu@gmail.com>
2022-05-16 10:52:31 +0200

    Co-authored-by: Runelc <Runelc@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ window.addEventListener("scroll", () => {
     outlines[0].style.top = (document.getElementById("program").offsetTop - scrollY-200)/37 + "%";
     outlines[1].style.top = (document.getElementById("billetter").offsetTop - scrollY+200)/34 + "%";
     outlines[2].style.top = (document.getElementById("kort").offsetTop - scrollY-200)/35 + "%";
+    outlines[3].style.top = (document.getElementById("merch").offsetTop - scrollY+200)/34 + "%";
 
     karrusselTop.style.backgroundPositionX = 100 + scrollY/20 + "%"
     karrusselBot.style.backgroundPositionX = "-" + scrollY/20 + "%"
