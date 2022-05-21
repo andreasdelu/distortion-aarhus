a24523899d9831aa6a4b17e550cb2954124d806a
andreasdelu <Andreasdelu@gmail.com>
2022-05-21 10:06:52 +0200

    program done, mangler kun data i json

--- js/program.js
+++ js/program.js
@@ -1,6 +1,13 @@
 const imgUrl = "../images/artister/cropped/";
 const modal = document.getElementById('artist-popup');
 
+const artistsFriday = document.querySelector('.friday');
+const artistsSaturday = document.querySelector('.saturday');
+const artistsSunday = document.querySelector('.sunday');
+const listFriday = document.querySelector('.list-friday');
+const listSaturday = document.querySelector('.list-saturday');
+const listSunday = document.querySelector('.list-sunday');
+
 async function fetchArtists() {
     const res = await fetch("../program/artists.json");
     const artists = await res.json();
@@ -11,24 +18,25 @@ fetchArtists()
 .then(data => populateDays(data))
 
 function populateDays(artists){
-    const artistsFriday = document.querySelector('.friday');
-    const artistsSaturday = document.querySelector('.saturday');
-    const artistsSunday = document.querySelector('.sunday');
     artists.friday.forEach(artist => {
-        artistsFriday.appendChild(createArtist(artist))
+        artistsFriday.appendChild(createGridArtist(artist))
+        listFriday.appendChild(createListArtist(artist))
     });
     artists.saturday.forEach(artist => {
-        artistsSaturday.appendChild(createArtist(artist))
+        artistsSaturday.appendChild(createGridArtist(artist))
+        listSaturday.appendChild(createListArtist(artist))
     });
     artists.sunday.forEach(artist => {
-        artistsSunday.appendChild(createArtist(artist))
+        artistsSunday.appendChild(createGridArtist(artist))
+        listSunday.appendChild(createListArtist(artist))
     });
 
 }
 
 const artistTemp = document.getElementById("artist-temp")
+const listTemp = document.getElementById("list-temp")
 
-function createArtist(artist){
+function createGridArtist(artist){
     const clone = artistTemp.content.cloneNode(true);
     const div = clone.querySelector(".artist");
     const name = clone.querySelector(".artist-name");
@@ -47,7 +55,27 @@ function createArtist(artist){
     return clone;
 }
 
+function createListArtist(artist){
+    const clone = listTemp.content.cloneNode(true);
+    const div = clone.querySelector(".list-item");
+    const name = clone.querySelector(".list-name");
+    const time = clone.querySelector(".list-time");
+
+    name.innerText = artist.name;
+    time.innerText = artist.day + " · " + artist.time;
+
+    div.addEventListener("click", () => {
+        createModal(artist);
+    })
+
+    return clone;
+}
+
 document.getElementById("modal-bg").addEventListener("click", () => {
+    modal.style.display = "none"
+})
+
+document.getElementById("modal-close").addEventListener("click", () => {
     modal.style.display = "none"
 })
 
@@ -81,3 +109,27 @@ function readMore() {
     document.getElementById("modal-info").style.paddingBottom = "25px";
 
 }
+
+const listBtn = document.getElementById('pref-list');
+const arrayBtn = document.getElementById('pref-array');
+
+listBtn.addEventListener("click", () => {
+    arrayBtn.classList.toggle("current-pref")
+    listBtn.classList.toggle("current-pref")
+    artistsFriday.style.display = "none"
+    artistsSaturday.style.display = "none"
+    artistsSunday.style.display = "none"
+    listFriday.style.display = "block"
+    listSaturday.style.display = "block"
+    listSunday.style.display = "block"
+})
+arrayBtn.addEventListener("click", () => {
+    arrayBtn.classList.toggle("current-pref")
+    listBtn.classList.toggle("current-pref")
+    artistsFriday.style.display = "grid"
+    artistsSaturday.style.display = "grid"
+    artistsSunday.style.display = "grid"
+    listFriday.style.display = "none"
+    listSaturday.style.display = "none"
+    listSunday.style.display = "none"
+})
